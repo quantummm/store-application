f7ba83889ea7a154774726c5f0498c12fbc05c33
quantummm <aidenwang0826@gmail.com>
2023-03-02 22:46:47 +1100

    pages added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,37 @@
+import { useEffect } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation
+} from 'react-router-dom';
+
+import  Home  from "./scenes/home/Home";
+import  Checkout  from "./scenes/checkout/Checkout";
+import  ItemDetails  from "./scenes/itemDetails/ItemDetails";
+
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() =>{
+    window.scrollTo(0, 0);
+  }, [pathname])
+
+  return null;
+}
+
 function App() {
   return (
-    <div className="App">
-
+    <div className="app">
+        <BrowserRouter>
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="item/:itemId" element={<ItemDetails />} />
+          <Route path="checkout" element={<Checkout />} />
+        </Routes>
+        </BrowserRouter>
     </div>
   );
 }
